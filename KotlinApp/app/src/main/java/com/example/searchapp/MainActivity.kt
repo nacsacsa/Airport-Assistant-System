5e57a1f76380194de7656eaf91163395c13853fc
Kolozsi Márton <kolozsi.ocsi@gmail.com>
2026-01-09 14:05:58 +0100

    Update MainActivity view

--- KotlinApp/app/src/main/java/com/example/searchapp/MainActivity.kt
+++ KotlinApp/app/src/main/java/com/example/searchapp/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.searchapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginButton : Button
+
+    private fun isPasswordGood(username : String, password : String): Boolean {
+        return username == "user" && password == "pass";
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        usernameInput = findViewById(R.id.input_username)
+        passwordInput = findViewById(R.id.input_password)
+        loginButton = findViewById(R.id.button_login)
+
+        loginButton.setOnClickListener{
+            val username = usernameInput.text.toString();
+            val password = passwordInput.text.toString();
+            if (isPasswordGood(username, password)) {
+                Log.i("Login attempt", "Siker");
+            }
+            else {
+                Log.i("Login attempt", "Fail");
+            }
         }
     }
 }
