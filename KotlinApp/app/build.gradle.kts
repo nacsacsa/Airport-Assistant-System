643e8b49b02e44d6db9fcdbd8c70b5e392c60844
Kolozsi Márton <kolozsi.ocsi@gmail.com>
2026-01-10 13:07:04 +0100

    Add json serialization and Http connection

--- KotlinApp/app/build.gradle.kts
+++ KotlinApp/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -37,11 +38,13 @@ android {
 
 dependencies {
 
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.support.annotations)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- KotlinApp/app/src/main/java/com/example/searchapp/SearchActivity.kt
+++ KotlinApp/app/src/main/java/com/example/searchapp/SearchActivity.kt
@@ -1,24 +1,36 @@
 package com.example.searchapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlinx.serialization.Serializable
+import java.net.HttpURLConnection
+import java.net.URL
 
 class SearchActivity : AppCompatActivity() {
 
+    @Serializable
+    data class Data(
+        val latitude: String,
+        val longitude: String
+    )
+
     lateinit var spinner : Spinner
     lateinit var searchButton : Button
+    //var url  = "https://geodb-free-service.wirefreethought.com/v1/geo/places?limit=1&offset=0&types=CITY&namePrefix=Budapest&sort=-population"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
 
+        searchButton = findViewById(R.id.button_submit)
+
+        var url  = "https://geodb-free-service.wirefreethought.com/v1/geo/places?limit=1&offset=0&types=CITY&namePrefix=Budapest&sort=-population"
         val items = listOf("Budapest", "Szeged", "Debrecen", "Győr", "Miskolc")
         spinner = findViewById(R.id.spinner_options)
         val adapter = ArrayAdapter(
@@ -29,5 +41,22 @@ class SearchActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
 
+        searchButton.setOnClickListener{
+            Thread {
+                val json = httpGet(url)
+                Log.i("JSON answer", json)
+            }.start()
+        }
+    }
+
+    fun httpGet(urlString: String) : String {
+        val url = URL(urlString)
+        val conn = url.openConnection() as HttpURLConnection
+        conn.requestMethod = "GET"
+        conn.connectTimeout = 5000
+        conn.readTimeout = 5000
+        val response = conn.inputStream.bufferedReader().use { it.readText() }
+        conn.disconnect()
+        return  response
     }
 }
