b014cf6536808f873d11d35ee8078e42cb448b9f
Kolozsi Márton <kolozsi.ocsi@gmail.com>
2026-01-10 16:57:45 +0100

    Add city search feature

--- KotlinApp/app/src/main/java/com/example/searchapp/SearchActivity.kt
+++ KotlinApp/app/src/main/java/com/example/searchapp/SearchActivity.kt
@@ -1,27 +1,39 @@
 package com.example.searchapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.Locale
+
 
 class SearchActivity : AppCompatActivity() {
 
     @Serializable
-    data class Data(
-        val latitude: String,
-        val longitude: String
+    data class GeoResponse(
+        val data: List<City>
+    )
+
+    @Serializable
+    data class City(
+        val name: String,
+        val latitude: Double,
+        val longitude: Double
     )
 
     lateinit var spinner : Spinner
     lateinit var searchButton : Button
-    //var url  = "https://geodb-free-service.wirefreethought.com/v1/geo/places?limit=1&offset=0&types=CITY&namePrefix=Budapest&sort=-population"
+
+    var baseUrl = "https://geodb-free-service.wirefreethought.com/v1/geo/places?limit=1&offset=0&types=CITY&sort=-population&namePrefix="
+    val items = listOf("Budapest", "Szeged", "Debrecen", "Győr", "Miskolc")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +41,6 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
 
         searchButton = findViewById(R.id.button_submit)
-
-        var url  = "https://geodb-free-service.wirefreethought.com/v1/geo/places?limit=1&offset=0&types=CITY&namePrefix=Budapest&sort=-population"
-        val items = listOf("Budapest", "Szeged", "Debrecen", "Győr", "Miskolc")
         spinner = findViewById(R.id.spinner_options)
         val adapter = ArrayAdapter(
             this,
@@ -42,21 +51,46 @@ class SearchActivity : AppCompatActivity() {
         spinner.adapter = adapter
 
         searchButton.setOnClickListener{
-            Thread {
-                val json = httpGet(url)
-                Log.i("JSON answer", json)
-            }.start()
+            searchActiveCity()
         }
+    }
+
+    private fun searchActiveCity() {
+        val selectedCity = spinner.selectedItem.toString()
+        val url = baseUrl + selectedCity
+        Thread {
+            val json = httpGet(url)
+            val response : GeoResponse = parseData(json)
+            val city = response.data.first()
+            val latitude = city.latitude
+            val longitude = city.longitude
+            runOnUiThread {
+                val uri = java.lang.String.format(
+                    Locale.ENGLISH,
+                    "geo:%f,%f?q=%f,%f", latitude, longitude, latitude, longitude
+                )
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+                startActivity(intent)
+            }
+        }.start()
     }
 
     fun httpGet(urlString: String) : String {
         val url = URL(urlString)
-        val conn = url.openConnection() as HttpURLConnection
-        conn.requestMethod = "GET"
-        conn.connectTimeout = 5000
-        conn.readTimeout = 5000
-        val response = conn.inputStream.bufferedReader().use { it.readText() }
-        conn.disconnect()
-        return  response
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        connection.connectTimeout = 5000
+        connection.readTimeout = 5000
+        val response = connection.inputStream.bufferedReader().use { it.readText() }
+        connection.disconnect()
+        return response
+    }
+
+    inline fun <reified T> parseData(json: String) : T {
+        val jsonParser = Json {
+            ignoreUnknownKeys = true
+        }
+        val response = jsonParser.decodeFromString<T>(json)
+        return response
     }
 }
