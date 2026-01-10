bdb4b9edd674e2c71fc2720fb1fcb184fe1be121
Kolozsi Márton <kolozsi.ocsi@gmail.com>
2026-01-10 17:11:57 +0100

    Add Spring backend to the project

---
+++ Airport-Asistant-backend/src/main/java/com/example/Airport/Asistant/backend/AirportAsistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Airport.Asistant.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportAsistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportAsistantBackendApplication.class, args);
+	}
+
+}
---
+++ Airport-Asistant-backend/src/test/java/com/example/Airport/Asistant/backend/AirportAsistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Airport.Asistant.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportAsistantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
