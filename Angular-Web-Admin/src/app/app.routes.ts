dd4b4bc42fdcd4fbccef5d36f3de760d65460487
Kolozsi Márton <kolozsi.ocsi@gmail.com>
2026-01-12 13:00:20 +0100

    Add Angular-Web-Admin frontend to the project

---
+++ Angular-Web-Admin/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HistoryComponent } from './history/history.component';
+
+export const routes: Routes = [
+    {path: '', component: HistoryComponent}
+];
