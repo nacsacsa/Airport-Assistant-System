78d64fc0157294c6bb9581d2ccd99680cd953c83
Kolozsi Márton <kolozsi.ocsi@gmail.com>
2026-01-12 16:51:00 +0100

    Add history service and history component logic implementation

--- Angular-Web-Admin/src/app/history.service.ts
+++ Angular-Web-Admin/src/app/history.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+
+export interface History{
+  timestamp: string;
+  username: string;
+  city: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class HistoryService {
+  private historyUrl : string = "http://localhost:8080/api/history";
+  constructor(private http : HttpClient) {}
 
-  constructor() { }
+  getHistory(): Observable<any>{
+      return this.http.get(this.historyUrl)
+  }
 }
--- Angular-Web-Admin/src/app/history/history.component.ts
+++ Angular-Web-Admin/src/app/history/history.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { History, HistoryService } from '../history.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-history',
-  imports: [],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.css'
+  imports: [CommonModule, MatTableModule],
+  styleUrl: './history.component.css',
+  standalone: true,
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit{
+  histories: any[] = []
+  displayedColumns : string[] = ['timestamp', 'username', "city"]
+  dataSource = new MatTableDataSource<History>(this.histories)
+
+  constructor(private historyService : HistoryService) {}
+  
+  ngOnInit(): void {
+    this.loadHistory()
+  }
+
+  loadHistory() {
+    this.historyService.getHistory().subscribe(data => {
+      this.histories = data
+      this.dataSource.data = data
+    })
+  }
 }
+
